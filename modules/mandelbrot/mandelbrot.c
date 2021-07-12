ba479d3ad71a90532f613c3f5ae2b97358fa9de2
Lucas Lima <lucasflima25@gmail.com>
2021-07-12 17:57:48 -0300

    Code refactored

--- modules/mandelbrot/mandelbrot.c
+++ modules/mandelbrot/mandelbrot.c
@@ -36,20 +36,20 @@ STATIC int get_colour(int colour, char colour_base) {
     return rgb_to_rgb565(0, 0, colour);
 }
 
-STATIC void madelbrot_readfrom(
-                                int width,
-                                int height, 
+STATIC void madelbrot_write_into(
+                                uint width,
+                                uint height, 
                                 float real_start, 
                                 float real_end, 
                                 float imaginary_end, 
                                 float imaginary_start, 
-                                int iterations,
+                                uint iterations,
                                 char colour_base,
                                 uint8_t* buffer
                             ) 
 {
     float real_part, imaginary_part;
-    int m, colour;
+    uint m, colour;
     for (int x = 0; x < width; x++) {
         for (int y = 0; y < height; y++) {
             real_part = real_start + ((float) x / (float) width) * (real_end - real_start);
@@ -64,17 +64,10 @@ STATIC void madelbrot_readfrom(
 }
 
 // Mandelbrot Python interface
-STATIC mp_obj_t mandelbrot(size_t n_args, const mp_obj_t *args) 
+STATIC void _mandelbrot_into(uint width, uint height, uint8_t* buffer, uint iterations, size_t n_args, const mp_obj_t *args) 
 {
     // Extract the tuple from the MicroPython input object
     mp_obj_t *tuple; 
-    
-    //Tuple of resolution
-    mp_obj_get_array_fixed_n(args[0], 2, &tuple);
-    int width = mp_obj_get_int(tuple[0]);
-    int height = mp_obj_get_int(tuple[1]);
-    
-    int diplay_size_buffer = width * height * 2;
 
     char * colour_base = (char *) mp_obj_str_get_str(args[1]);
 
@@ -85,14 +78,28 @@ STATIC mp_obj_t mandelbrot(size_t n_args, const mp_obj_t *args)
     float imaginary_start = mp_obj_get_float(tuple[2]);
     float imaginary_end = mp_obj_get_float(tuple[3]);
 
+    madelbrot_write_into(width, height, real_start, real_end, imaginary_start, imaginary_end, iterations, colour_base[0], buffer);
+}
+
+// Mandelbrot Python interface
+STATIC mp_obj_t mandelbrot(size_t n_args, const mp_obj_t *args) 
+{
+    mp_obj_t *tuple; 
+    
+    //Tuple of resolution
+    mp_obj_get_array_fixed_n(args[0], 2, &tuple);
+    uint width = mp_obj_get_int(tuple[0]);
+    uint height = mp_obj_get_int(tuple[1]);
+
+    uint diplay_size_buffer = width * height * 2;
+
     uint iterations = 10;
     if(n_args == 4) {
         iterations = mp_obj_get_int(args[3]);
     }
 
     uint8_t* buffer = (uint8_t*) m_malloc(diplay_size_buffer);
-
-    madelbrot_readfrom(width, height, real_start, real_end, imaginary_start, imaginary_end, iterations, colour_base[0], buffer);
+    _mandelbrot_into(width, height, buffer, iterations, n_args, args);
 
     return mp_obj_new_bytearray_by_ref(diplay_size_buffer, buffer);
 }
@@ -102,22 +109,12 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mandelbrot_obj, 3, 4, mandelbrot);
 // Mandelbrot write into buffer Python interface. Avoid allocation
 STATIC mp_obj_t mandelbrot_into(size_t n_args, const mp_obj_t *args) 
 {
-    // Extract the tuple from the MicroPython input object
     mp_obj_t *tuple; 
     
     //Tuple of resolution
     mp_obj_get_array_fixed_n(args[0], 2, &tuple);
     int width = mp_obj_get_int(tuple[0]);
     int height = mp_obj_get_int(tuple[1]);
-
-    char * colour_base = (char *) mp_obj_str_get_str(args[1]);
-
-    //Tuple of boundries
-    mp_obj_get_array_fixed_n(args[2], 4, &tuple);
-    float real_start = mp_obj_get_float(tuple[0]);
-    float real_end = mp_obj_get_float(tuple[1]);
-    float imaginary_start = mp_obj_get_float(tuple[2]);
-    float imaginary_end = mp_obj_get_float(tuple[3]);
 
     //Buffer to write into
     mp_buffer_info_t bufinfo;
@@ -132,7 +129,7 @@ STATIC mp_obj_t mandelbrot_into(size_t n_args, const mp_obj_t *args)
         iterations = mp_obj_get_int(args[4]);
     }
 
-    madelbrot_readfrom(width, height, real_start, real_end, imaginary_start, imaginary_end, iterations, colour_base[0], bufinfo.buf);
+    _mandelbrot_into(width, height, bufinfo.buf, iterations, n_args, args);
 
     return mp_obj_new_int(bufinfo.len);
 }
